7016da729231637955f9ffcae9c9e497e29ef4ff
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-20 00:02:15 -0300

    improve gamma

--- ProgramasMicroPython/vac/gamma.py
+++ ProgramasMicroPython/vac/gamma.py
@@ -3,7 +3,7 @@ import math
 def fact(n):
     res = 1
     while n > 0:
-        res = res * n
+        res *= n
         n -= 1
     return res
 
@@ -24,9 +24,19 @@ def gamma_cdf(x, n, lam):
         return 0
 
     # CDF: 1 - sum_{k=0}^{n-1} [(lambda*x)^k * e^(-lambda*x)] / k!
+    # Compute e^(-lambda*x) once outside the loop
+    exp_term = math.exp(-lam * x)
     suma = 0.0
-    for k in range(n):
-        suma += ((lam * x)**k * math.exp(-lam * x)) / fact(k)
+    lam_x = lam * x
+
+    # Use iterative calculation to avoid repeated factorial computations
+    term = exp_term  # k=0: (lam*x)^0 / 0! * exp(-lam*x) = exp(-lam*x)
+    suma = term
+
+    for k in range(1, n):
+        term = term * lam_x / k  # (lam*x)^k / k! * exp(-lam*x)
+        suma += term
+
     return 1 - suma
 
 x = float(input("Ingrese x: "))
